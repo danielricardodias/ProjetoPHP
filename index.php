8b7dc119987e9c89f29b38ece6d9059598a1b57d
laboratorio <laboratorio@EP2473.academico.projecao>
2019-05-27 08:40:58 -0300

    Teste Primeiro commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+Projeto PHP aula Aplicações WEB, fazer upload do projeto no GIT
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
